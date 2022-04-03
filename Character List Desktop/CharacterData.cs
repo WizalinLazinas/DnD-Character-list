d97cd19186d8f9a11a0356e9abeca57b2e8a597c
WizalinLazinas <kmelnikov55555@gmail.com>
2022-04-03 20:43:10 +0300

    Initial commit

---
+++ Character List Desktop/CharacterData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_List_Desktop
+{
+    internal class CharacterData
+    {
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Class { get; set; }
+        public string Background { get; set; }
+        public string Dragonborn { get; set; }
+        public string Gnome { get; set; }
+        public string HalfElf { get; set; }
+        public string HalfOrc { get; set; }
+        public string Tiefling { get; set; }
+    }
+}
---
+++ Character List Desktop/RaceBasics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_List_Desktop
+{
+    internal class RaceBasics
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Characteristics Characteristics { get; set; }
+        public string Features { get; set; }
+        public string Skills { get; set; }
+        public string Relationships { get; set; }
+
+    }
+}
